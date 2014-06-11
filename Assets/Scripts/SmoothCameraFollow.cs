fbe3dbec3b7964dfc40ae469cea3f1193820d4f4
mariocatch <mariocatch@gmail.com>
2014-06-11 15:43:34 -0400

    Removing unused properties, adding tunables to camera follow script.

--- Assets/Scripts/SmoothCameraFollow.cs
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -2,14 +2,12 @@
 
 public class SmoothCameraFollow : MonoBehaviour
 {
+    public float BackwardsDistanceFromPlayer = -15.0f;
     public float DampAmount = 6.0f;
-    public float MinimumDistance = 10.0f;
+    public float ElevatedDistanceFromPlayer = 15.0f;
     public Transform PlayerTransform;
-    public bool ShouldSmooth = true;
-    private float mAlpha = 1.0f;
-    private Color mColor;
     private Transform mThisTransform;
-    
+
 
     private void Awake()
     {
@@ -18,30 +16,11 @@ public class SmoothCameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (PlayerTransform)
+        if (PlayerTransform != null)
         {
-            if (ShouldSmooth)
-            {
-                Quaternion rotation = Quaternion.LookRotation(PlayerTransform.position - mThisTransform.position);
-                mThisTransform.rotation = Quaternion.Slerp(mThisTransform.rotation, rotation, Time.deltaTime*DampAmount);
-                mThisTransform.position = Vector3.Slerp(transform.position, PlayerTransform.position + new Vector3(0f, (15 - PlayerTransform.position.y), -15.0f), Time.deltaTime*2.0f);
-            }
-            else
-            {
-                mThisTransform.rotation = Quaternion.FromToRotation(-Vector3.forward, (new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, PlayerTransform.position.z) - mThisTransform.position).normalized);
-                mThisTransform.position = Vector3.Slerp(transform.position, PlayerTransform.position + new Vector3(0f, (15 - PlayerTransform.position.y), -15.0f), Time.deltaTime * 2.0f);
-
-                float distance = Vector3.Distance(PlayerTransform.position, mThisTransform.position);
-
-                if (distance < MinimumDistance)
-                {
-                    mAlpha = Mathf.Lerp(mAlpha, 0.0f, Time.deltaTime*2.0f);
-                }
-                else
-                {
-                    mAlpha = Mathf.Lerp(mAlpha, 1.0f, Time.deltaTime*2.0f);
-                }
-            }
+            Quaternion rotation = Quaternion.LookRotation(PlayerTransform.position - mThisTransform.position);
+            mThisTransform.rotation = Quaternion.Slerp(mThisTransform.rotation, rotation, Time.deltaTime*DampAmount);
+            mThisTransform.position = Vector3.Slerp(transform.position, PlayerTransform.position + new Vector3(0f, (15 - PlayerTransform.position.y), -15.0f), Time.deltaTime*2.0f);
         }
     }
 }
