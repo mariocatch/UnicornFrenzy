4f708f02dd8bb90ada264244b7d6e70a8f14511e
mariocatch <mariocatch@gmail.com>
2014-06-11 12:33:43 -0400

    Temporary floor, scene light, player movement, and perspective camera.

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private float DestinationDistance;
+    public float MovementSpeed;
+    private Transform mPlayerTransform;
+    private Vector3 mTargetDestination;
+
+
+    private void Start()
+    {
+        mPlayerTransform = transform;
+        mTargetDestination = mPlayerTransform.position;
+    }
+
+    private void Update()
+    {
+        DestinationDistance = Vector3.Distance(mTargetDestination, mPlayerTransform.position);
+
+        if (DestinationDistance < .5f)
+        {
+            MovementSpeed = 0;
+        }
+        else if (DestinationDistance > .5f)
+        {
+            MovementSpeed = 3;
+        }
+
+
+        // Moves the Player if the Left Mouse Button was clicked
+        if (Input.GetMouseButtonDown(0) && GUIUtility.hotControl == 0)
+        {
+            var playerPlane = new Plane(Vector3.up, mPlayerTransform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitdist = 0.0f;
+
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                mTargetDestination = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                mPlayerTransform.rotation = targetRotation;
+            }
+        }
+
+        //    // Moves the player if the mouse button is hold down
+        //else if (Input.GetMouseButton(0) && GUIUtility.hotControl == 0)
+        //{
+        //    var playerPlane = new Plane(Vector3.up, mPlayerTransform.position);
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    float hitdist = 0.0f;
+
+        //    if (playerPlane.Raycast(ray, out hitdist))
+        //    {
+        //        Vector3 targetPoint = ray.GetPoint(hitdist);
+        //        mTargetDestination = ray.GetPoint(hitdist);
+        //        Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+        //        mPlayerTransform.rotation = targetRotation;
+        //    }
+        //    //	mPlayerTransform.position = Vector3.MoveTowards(mPlayerTransform.position, mTargetDestination, moveSpeed * Time.deltaTime);
+        //}
+
+        // To prevent code from running if not needed
+        if (DestinationDistance > .5f)
+        {
+            mPlayerTransform.position = Vector3.MoveTowards(mPlayerTransform.position, mTargetDestination, MovementSpeed*Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public float DampAmount = 6.0f;
+    public float MinimumDistance = 10.0f;
+    public Transform PlayerTransform;
+    public bool ShouldSmooth = true;
+    private float mAlpha = 1.0f;
+    private Color mColor;
+    private Transform mThisTransform;
+
+    private void Awake()
+    {
+        mThisTransform = transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (PlayerTransform)
+        {
+            if (ShouldSmooth)
+            {
+                Quaternion rotation = Quaternion.LookRotation(PlayerTransform.position - mThisTransform.position);
+                mThisTransform.rotation = Quaternion.Slerp(mThisTransform.rotation, rotation, Time.deltaTime*DampAmount);
+            }
+            else
+            {
+                mThisTransform.rotation = Quaternion.FromToRotation(-Vector3.forward, (new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, PlayerTransform.position.z) - mThisTransform.position).normalized);
+
+                float distance = Vector3.Distance(PlayerTransform.position, mThisTransform.position);
+
+                if (distance < MinimumDistance)
+                {
+                    mAlpha = Mathf.Lerp(mAlpha, 0.0f, Time.deltaTime*2.0f);
+                }
+                else
+                {
+                    mAlpha = Mathf.Lerp(mAlpha, 1.0f, Time.deltaTime*2.0f);
+                }
+            }
+        }
+    }
+}
