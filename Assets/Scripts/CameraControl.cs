5931f5e9f4f9ba612c1f70e638fe5d34c99bdf04
Thelohel <mboshu@gmail.com>
2014-06-22 10:28:12 -0400

    Added moving camera (move mouse to the edge of the screen in the direction you want to move).

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public int Boundary = 50;
+	public int Speed = 50;
+
+	private int mTheScreenWidth;
+	private int mTheScreenHeight;
+	private Vector3 mPosHolder;
+
+	void Start(){
+
+		mTheScreenWidth = Screen.width;
+		mTheScreenHeight = Screen.height;
+
+		}
+
+	void Update(){
+
+		if (Input.mousePosition.x > mTheScreenWidth - Boundary) {
+			mPosHolder = transform.position;
+			mPosHolder.x += Speed * Time.deltaTime;
+			transform.position = mPosHolder;
+				}
+
+		if (Input.mousePosition.x < 0 + Boundary) {
+			mPosHolder = transform.position;
+			mPosHolder.x -= Speed * Time.deltaTime;
+			transform.position = mPosHolder;
+				}
+
+		if (Input.mousePosition.y > mTheScreenHeight - Boundary) {
+			mPosHolder = transform.position;
+			mPosHolder.z += Speed * Time.deltaTime;
+			transform.position = mPosHolder;
+				}
+
+		if (Input.mousePosition.y < 0 + Boundary) {
+			mPosHolder = transform.position;
+			mPosHolder.z -= Speed * Time.deltaTime;
+			transform.position = mPosHolder;
+				}
+
+	}
+}
